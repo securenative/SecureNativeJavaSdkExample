424599f0e68b75aee3d94f8e488590e8d33b1670
nevo <nevo@securenative.com>
2019-06-23 19:41:42 +0300

    added config for filters endpoints for filters

--- src/main/java/com/securenative/java/sdk/example/securenativejavasdkexample/Controller.java
+++ src/main/java/com/securenative/java/sdk/example/securenativejavasdkexample/Controller.java
@@ -1,17 +1,22 @@
 package com.securenative.java.sdk.example.securenativejavasdkexample;
 
 import exceptions.SecureNativeSDKException;
+import javafx.util.Pair;
 import models.Device;
 import models.EventOptions;
 import models.SecureNativeOptions;
 import models.User;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import snlogic.EventTypes;
 import snlogic.SecureNative;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -26,36 +31,32 @@ public class Controller {
     User user = new User("1","Bruce Wayne","bruce@wayne.com");
     String cookie = "cookie";
     String loginEvent = EventTypes.LOG_IN.getType();
-    String passwordResetType = EventTypes.PASSWORD_RESET.getType();
+    String passwordResetType = EventTypes.VERIFY.getType();
     String logoutEvent = EventTypes.LOG_OUT.getType();
+    List ts = Arrays.asList(new Pair<>("param", "paramValue"));
 
 
     @RequestMapping("/track")
-    public String track( HttpServletRequest request, HttpServletResponse response) {
+    public String track(HttpServletRequest request, HttpServletResponse response) {
         try {
             secureNative = new SecureNative(API_KEY,new SecureNativeOptions());
         } catch (SecureNativeSDKException e) {
             e.printStackTrace();
             return "Api key is not valid";
         }
-        try {
-            secureNative.track(new EventOptions(ip,remoteIP,userAgent,device,user,cookie,loginEvent, Collections.singletonMap("param", "paramValue")),request);
-        } catch (SecureNativeSDKException e) {
-            e.printStackTrace();
-            return "You can only specify maximum of 6 params";
-        }
+        secureNative.track(new EventOptions("103.28.53.138", remoteIP, userAgent, device, user, cookie, loginEvent, ts), request);
         return "tracked";
     }
 
     @RequestMapping("/verify")
-    public String verify( HttpServletRequest request, HttpServletResponse response) {
+    public String verify(HttpServletRequest request, HttpServletResponse response) {
         try {
             secureNative = new SecureNative(API_KEY,new SecureNativeOptions());
         } catch (SecureNativeSDKException e) {
             e.printStackTrace();
             return "Api key is not valid";
         }
-        secureNative.verify(new EventOptions(ip,remoteIP,userAgent,device,user,cookie,passwordResetType, Collections.singletonMap("param", "paramValue")),request);
+        secureNative.verify(new EventOptions("103.28.53.138",remoteIP,userAgent,device,user,cookie,passwordResetType, ts),request);
         return "verify";
     }
 
@@ -67,7 +68,23 @@ public class Controller {
             e.printStackTrace();
             return "Api key is not valid";
         }
-        secureNative.flow(flowId,new EventOptions(ip,remoteIP,userAgent,device,user,cookie, logoutEvent, Collections.singletonMap("param", "paramValue")),request);
+        secureNative.flow(flowId,new EventOptions(ip,remoteIP,userAgent,device,user,cookie, logoutEvent,ts),request);
         return "flow";
     }
+
+    @RequestMapping("/webhook")
+    public String webhook( HttpServletRequest request, HttpServletResponse response) {
+        System.out.printf("success");
+        return "success";
+    }
+
+
+    @RequestMapping(value ="/webhook" , method = RequestMethod.POST)
+    public @ResponseBody
+    String addNewWorker(@RequestBody String jsonString) {
+
+        //do business logic
+        return "1";
+    }
+
 }
